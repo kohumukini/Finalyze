d3739fdf633f3d9e94293ba52dc941a7d11de8a7
kohumukini <kealii.ohumukini@gmail.com>
2026-07-28 22:03:37 -0700

    Fetch adjustment

--- frontend/chat.js
+++ frontend/chat.js
@@ -28,7 +28,7 @@ chatForm.addEventListener('submit', async (event) => {
   chatConversation.scrollTop = chatConversation.scrollHeight;
 
   try {
-    const response = await fetch('http://127.0.0.1:8000/chat/model', {
+    const response = await fetch('/chat/model', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
